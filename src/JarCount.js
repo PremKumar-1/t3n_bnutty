e8db336ed2b297312a436e1ed392060fc06f1ce8
Prem Kumar <125431380+PremKumar2026@users.noreply.github.com>
2024-07-10 19:41:15 -0500

    Update JarCount.js

--- src/JarCount.js
+++ src/JarCount.js
@@ -15,7 +15,18 @@ const Dashboard = () => {
     const [inventory, setInventory] = useState([]);
 
     useEffect(() => {
-        const fetchJarCount = async () => {
+        const socket = new WebSocket('ws://3.129.44.247/ws/jarcounts/');
+
+        socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            // Update your state with the received data
+        };
+
+        return () => socket.close();
+    }, []);
+
+    useEffect(() => {
+        const fetchInitialData = async () => {
             try {
                 const response = await fetch(`/api/jarcounts/?date=${date}`);
                 const data = await response.json();
@@ -31,30 +42,16 @@ const Dashboard = () => {
                     total += item.count;
                 });
                 setJarCount({ shift1, shift2, total });
+
+                const inventoryResponse = await fetch(`/api/inventories/`);
+                const inventoryData = await inventoryResponse.json();
+                setInventory(inventoryData);
             } catch (error) {
-                console.error("Error fetching jar count:", error);
+                console.error("Error fetching initial data:", error);
             }
         };
 
-        const fetchInventory = async () => {
-            try {
-                const response = await fetch(`/api/inventories/`);
-                const data = await response.json();
-                setInventory(data);
-            } catch (error) {
-                console.error("Error fetching inventory:", error);
-            }
-        };
-
-        fetchJarCount();
-        fetchInventory();
-
-        const interval = setInterval(() => {
-            fetchJarCount();
-            fetchInventory();
-        }, 250); // Refresh 4 times per second
-
-        return () => clearInterval(interval); // Clean up on unmount
+        fetchInitialData();
     }, [date]);
 
     const handleDateChange = (e) => {
@@ -116,4 +113,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-

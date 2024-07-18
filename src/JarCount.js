ccaa6e9187ee92e0d999754f0744be8e0a029ceb
Prem Kumar <125431380+PremKumar2026@users.noreply.github.com>
2024-07-17 20:30:59 -0500

    Update JarCount.js

--- src/JarCount.js
+++ src/JarCount.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './JarCount.css';
+import Speedometer from './Speedometer';
 
 const Dashboard = () => {
     const getCurrentDate = () => {
@@ -13,48 +14,70 @@ const Dashboard = () => {
     const [date, setDate] = useState(getCurrentDate());
     const [jarCount, setJarCount] = useState({ shift1: 0, shift2: 0, total: 0 });
     const [inventory, setInventory] = useState([]);
+    const [jarsPerHour, setJarsPerHour] = useState(0);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        const fetchJarCount = async () => {
+        const fetchData = async () => {
+            setLoading(true);
             try {
-                const response = await fetch(`/api/jarcounts/?date=${date}`);
-                const data = await response.json();
-                let shift1 = 0;
-                let shift2 = 0;
-                let total = 0;
-                data.forEach(item => {
-                    if (item.shift === 'day') {
-                        shift1 += item.count;
-                    } else if (item.shift === 'night') {
-                        shift2 += item.count;
-                    }
-                    total += item.count;
-                });
-                setJarCount({ shift1, shift2, total });
+                const jarCountPromise = fetchAllJarCounts();
+                const inventoryPromise = fetchInventory();
+
+                await Promise.all([jarCountPromise, inventoryPromise]);
+
+                setLoading(false);
             } catch (error) {
-                console.error("Error fetching jar count:", error);
+                console.error("Error fetching data:", error);
+                setLoading(false);
             }
+        };
+
+        const fetchAllJarCounts = async () => {
+            let jarCounts = [];
+            let nextPageUrl = `/api/jarcounts/?date=${date}`;
+            
+            while (nextPageUrl) {
+                const response = await fetch(nextPageUrl);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                jarCounts = jarCounts.concat(data.results);
+                nextPageUrl = data.next;
+            }
+
+            const shift1 = jarCounts.filter(count => count.shift === 'day').length;
+            const shift2 = jarCounts.filter(count => count.shift === 'night').length;
+            const total = shift1 + shift2;
+            setJarCount({ shift1, shift2, total });
+
+            const hoursWorked = 12;
+            const jarsPerHour = total / hoursWorked;
+            setJarsPerHour(isNaN(jarsPerHour) ? 0 : jarsPerHour);
         };
 
         const fetchInventory = async () => {
             try {
                 const response = await fetch(`/api/inventories/`);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
                 const data = await response.json();
-                setInventory(data);
+                const inventoryData = data.results;
+                if (Array.isArray(inventoryData)) {
+                    setInventory(inventoryData);
+                } else {
+                    console.error("Error: Inventory data is not an array", data);
+                    setInventory([]);
+                }
             } catch (error) {
                 console.error("Error fetching inventory:", error);
+                setInventory([]);
             }
         };
 
-        fetchJarCount();
-        fetchInventory();
-
-        const interval = setInterval(() => {
-            fetchJarCount();
-            fetchInventory();
-        }, 1000); // Refresh 4 times per second
-
-        return () => clearInterval(interval); // Clean up on unmount
+        fetchData();
     }, [date]);
 
     const handleDateChange = (e) => {
@@ -71,46 +94,59 @@ const Dashboard = () => {
                 value={date}
                 onChange={handleDateChange} 
             />
-            <h2>Main Room Jar Count (RITA)</h2>
-            <table className="data-table">
-                <thead>
-                    <tr>
-                        <th>Shift</th>
-                        <th>Completed</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Shift 1</td>
-                        <td>{jarCount.shift1}</td>
-                    </tr>
-                    <tr>
-                        <td>Shift 2</td>
-                        <td>{jarCount.shift2}</td>
-                    </tr>
-                    <tr>
-                        <td>Total</td>
-                        <td>{jarCount.total}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <h2>Inventory</h2>
-            <table className="data-table">
-                <thead>
-                    <tr>
-                        <th>Item</th>
-                        <th>Quantity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {inventory.map((item, index) => (
-                        <tr key={index}>
-                            <td>{item.product_name ? item.product_name.trim() : 'Unknown'}</td>
-                            <td>{item.quantity.toFixed(2)}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    <Speedometer value={jarsPerHour} />
+                    <h2>Main Room Jar Count (RITA)</h2>
+                    <table className="data-table">
+                        <thead>
+                            <tr>
+                                <th>Shift</th>
+                                <th>Completed</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Shift 1</td>
+                                <td>{jarCount.shift1}</td>
+                            </tr>
+                            <tr>
+                                <td>Shift 2</td>
+                                <td>{jarCount.shift2}</td>
+                            </tr>
+                            <tr>
+                                <td>Total</td>
+                                <td>{jarCount.total}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <h2>Inventory</h2>
+                    <table className="data-table">
+                        <thead>
+                            <tr>
+                                <th>Item</th>
+                                <th>Quantity</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Array.isArray(inventory) && inventory.length > 0 ? (
+                                inventory.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>{item.product_name ? item.product_name.trim() : 'Unknown'}</td>
+                                        <td>{item.quantity.toFixed(2)}</td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="2">No inventory data available</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </>
+            )}
         </div>
     );
 };
